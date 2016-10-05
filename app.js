bf0d52c666e5335df0667c41cc7f2801b1f3c88e
Flo <florian.michel69250@gmail.com>
2016-10-05 21:55:49 +0200

    added express

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-var http = require("http");
 var fs = require("fs");
+var app = require('express')();
+var server = require("http").Server(app);
+
+app.set('port', process.env.PORT || 3000);
 
 var Session = require('express-session');
 var SessionStore = require("session-file-store")(Session);
 var session = Session({ store : new SessionStore({ path : 'tmp/sessions'}), secret : 'pass', resave : true, saveUninitialized : true});
 
-var server = http.createServer(function (req, res) {
-    fs.readFile('./index.html', 'utf-8', function (error, content) {
-        res.writeHead(200, {'Content-Type' : 'text/html'});
-        res.end(content);
+app.get('/', function (req, res) {
+    res.sendFile('index.html', {root: __dirname} );
+})
+    .use(function (req, res, next) {
+        res.setHeader('Content-Type', 'text/plain');
+        res.status(404).send('page introuvable');
     });
-});
+// var server = http.createServer(function (req, res) {
+//     fs.readFile('./index.html', 'utf-8', function (error, content) {
+//         res.writeHead(200, {'Content-Type' : 'text/html'});
+//         res.end(content);
+//     });
+// });
 
 var ios = require('socket.io-express-session');
-var io = require('socket.io').listen(server);
+var io = require('socket.io')(server);
 
 io.use(ios(session));
 
@@ -48,5 +58,5 @@ io.sockets.on("connection", function (socket) {
     });
 });
 
-// server.listen(3000);
-server.listen(process.env.PORT);
+server.listen(app.get('port'));
+// app.listen(process.env.PORT);
