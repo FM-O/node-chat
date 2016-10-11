6c365e9c7d80ce0cb35535670e672f551e267e09
Flo <florian.michel69250@gmail.com>
2016-10-11 17:19:40 +0200

    big update

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 var fs = require("fs");
 var app = require('express')();
-var server = require("http").Server(app);
+var server = require("http").createServer(app);
+var io = require('socket.io')(server);
+var ios = require('socket.io-express-session');
 
 app.set('port', process.env.PORT || 3000);
 
 var Session = require('express-session');
-var SessionStore = require("session-file-store")(Session);
-var session = Session({ store : new SessionStore({ path : 'tmp/sessions'}), secret : 'pass', resave : true, saveUninitialized : true});
+var sessionStore = new Session.MemoryStore();
+var session = Session({ name: 'sid', store : sessionStore, secret: 'secret', saveUninitialized: true, resave: true });
+
+io.use(ios(session));
+app.use(session);
 
 app.get('/', function (req, res) {
+    req.session.uid = Date.now();
+
     res.sendFile('public/index.html', {root: __dirname} );
 })
     .use(function (req, res, next) {
@@ -16,18 +23,16 @@ app.get('/', function (req, res) {
         res.status(404).send('page introuvable');
     });
 
-var ios = require('socket.io-express-session');
-var io = require('socket.io')(server);
-
-io.use(ios(session));
-
 io.sockets.on("connection", function (socket) {
 
     ///////// LOGS //////////
-    socket.handshake.session.uid = Date.now();
+    // console.log(socket.handshake.session.pseudo);
     ///////// LOGS //////////
 
-    socket.emit('welcome', 'vous êtes bien connecté');
+    if (socket.handshake.session.pseudo !== undefined) {
+        socket.broadcast.emit('new_arrived', socket.handshake.session.pseudo + ' vient de se connecter !');
+    }
+
     socket.handshake.session.messages = [];
     
     socket.on('message', function (message) {
@@ -43,12 +48,29 @@ io.sockets.on("connection", function (socket) {
     });
 
     socket.on('petit_nouveau', function(pseudo) {
-        socket.handshake.session.pseudo = pseudo;
+        var sessionsKeys = Object.keys(socket.handshake.sessionStore.sessions);
+        var socketSession = socket.handshake.sessionID;
+
+        if (sessionsKeys[sessionsKeys.length - 1] !== socketSession) {
+            socket.handshake.sessionID = sessionsKeys[sessionsKeys.length - 1];
+        }
+
+        sessionStore.load(socket.handshake.sessionID, function (err, sess) {
+
+            sess.pseudo = pseudo;
+
+            socket.handshake.session.pseudo = sess.pseudo;
+
+            sessionStore.set(socket.handshake.sessionID, sess, function () {
+                console.log("saved");
+            });
+        });
+        socket.emit('welcome', 'vous êtes bien connecté');
         socket.broadcast.emit('new_arrived', pseudo + ' vient de se connecter !');
     });
 
     socket.on('disconnect', function () {
-       socket.broadcast.emit('user_disconnected', socket.handshake.session.pseudo + ' vient de se déconnecter');
+        socket.broadcast.emit('user_disconnected', socket.handshake.session.pseudo + ' vient de se déconnecter');
     });
 });
 
